a891f5ebadb0bf85a75dc622bec0766f39cf4e6d
joaquin escamilla <95460965+JQ3417@users.noreply.github.com>
2025-06-02 15:06:25 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,56 @@
-from ultralytics import YOLO
-import multiprocessing
+# recorte_publicador_camera.py
 import cv2
-import torch
+import numpy as np
 
-torch.backends.cudnn.benchmark = True
-torch.backends.cudnn.enabled = False
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+
+class RecortePublisherCam(Node):
+    def __init__(self):
+        super().__init__('recorte_publisher_camera')
+
+        self.publisher = self.create_publisher(Image, 'imagen_recortada', 10)
+        self.bridge = CvBridge()
+
+        # Captura de cámara local (0 = webcam por defecto)
+        self.cap = cv2.VideoCapture(0)
+        if not self.cap.isOpened():
+            self.get_logger().error('No se pudo abrir la cámara.')
+            exit()
+
+        self.timer = self.create_timer(0.1, self.timer_callback)
+
+    def timer_callback(self):
+        ret, frame = self.cap.read()
+        if not ret:
+            self.get_logger().warning('No se pudo leer un frame de la cámara.')
+            return
+
+        frame = cv2.resize(frame, (640, 480))
+        new_w, new_h = 320, 80
+        x0 = (640 - new_w) // 2
+        cropped = frame[0:200, x0:x0 + new_w]
+
+        # Convertir BGR (OpenCV) a RGB (para 'rgb8')
+        cropped_rgb = cv2.cvtColor(cropped, cv2.COLOR_BGR2RGB)
+
+        msg = self.bridge.cv2_to_imgmsg(cropped_rgb, encoding='rgb8')
+        self.publisher.publish(msg)
+
+    def destroy_node(self):
+        self.cap.release()
+        super().destroy_node()
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = RecortePublisherCam()
+    try:
+        rclpy.spin(node)
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
 
 if __name__ == '__main__':
-    multiprocessing.freeze_support() # Ensure this is the first line in the if block
-
-    # Load a model
-    model = YOLO("best_12v_61.pt").to("cuda")  # Load a model from file
-
-    # Use the model
-    model.train(data="config.yaml",
-                 epochs=100,
-                 imgsz=512,
-                 batch=8,
-                 device="cuda",
-                 optimizer="AdamW",
-                 patience=10,
-                 amp= False,
-                 #workers=8,
-                 lr0=0.001,
-                 weight_decay=0.0005,
-                 augment=False)  # Train the model
-
-    metrics = model.val()  # evaluate model performance on the validation set
+    main()
